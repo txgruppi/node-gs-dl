31f38f48265163bec44134d91a93e25222077ab5
Tarcisio Gruppi <txgruppi@gmail.com>
2013-12-14 02:06:18 -0200

    Added exports to index.js and updated package.js

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ var CLI = require('./lib/cli');
 
 if (require.main === module) {
   CLI.run(process.argv);
+} else {
+  module.exports.GSDL = require('./lib/gs-dl');
+  module.exports.CLI = require('./lib/cli');
 }
